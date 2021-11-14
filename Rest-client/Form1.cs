a5fa1fa6dd36d3cb85ec2ea48ba21b2930d876e0
SanderTLN <62286818+SanderTLN@users.noreply.github.com>
2021-11-14 18:52:59 +0200

    Add files via upload

---
+++ Rest-client/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rest_client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdGO_Click(object sender, EventArgs e)
+        {
+            RESTClient rClient = new RESTClient();
+
+            rClient.endPoint = txtRequestURI.Text;
+            debugOutput("RESTClient Object created.");
+
+            string strJSON = string.Empty;
+
+            strJSON = rClient.makeRequest();
+
+            debugOutput(strJSON);
+        }
+
+        private void debugOutput(string strDebugText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strDebugText + Environment.NewLine);
+                txtResponse.Text = txtResponse.Text + strDebugText + Environment.NewLine;
+                txtResponse.SelectionStart = txtResponse.TextLength;
+                txtResponse.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message, ToString() + Environment.NewLine);
+            }
+        }
+
+        private void TxtRequestURI_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
